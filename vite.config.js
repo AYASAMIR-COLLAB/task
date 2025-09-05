ae8076358f53b0d88f2f625a0535ffd8fa6f3803
aya_samir <ayamohamedsamirrr@gmail.com>
2025-09-06 01:56:18 +0300

    Build successful with correct base configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/atm-app/', 
+  base: '/task/', // استخدم اسم مستودعك الصحيح
 })
